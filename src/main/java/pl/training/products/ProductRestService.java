5ca3c58d4b0a97cb3ee5e20a808048370947e525
Marcin Chrost <mchrost82@gmail.com>
2022-12-11 16:26:58 +0100

    getProduct operation

--- src/main/java/pl/training/products/ProductRestService.java
+++ src/main/java/pl/training/products/ProductRestService.java
@@ -1,6 +1,7 @@
 package pl.training.products;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -8,6 +9,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.mapstruct.factory.Mappers;
@@ -38,5 +40,14 @@ public class ProductRestService {
         Product productToAdd = productMapper.fromInputDto(productInputDto);
         Product product = productRepository.save(productToAdd);
         return productMapper.toOutputDto(product);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ProductOutputDto getProduct(@PathParam("id") Long id) {
+        return Optional.ofNullable(productRepository.findById(id))
+            .map(productMapper::toOutputDto)
+            .orElseThrow(RuntimeException::new);
     }
 }
