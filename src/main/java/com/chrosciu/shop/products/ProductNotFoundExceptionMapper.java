venustuspl/shop-jee-sages-training

Simple exception mapper for JAX-RS (if product is not found)

src/main/java/com/chrosciu/shop/products/ProductNotFoundExceptionMapper.java
@@ -1,0 +1,15 @@
+package com.chrosciu.shop.products;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ProductNotFoundExceptionMapper implements ExceptionMapper<ProductNotFoundException> {
+
+    @Override
+    public Response toResponse(ProductNotFoundException exception) {
+        return Response.status(Status.BAD_REQUEST).build();
+    }
+}

chrosciu/shop-jee

(cherry picked from commit f6c5291af129b06e8e3d90dc584ca48cf9af782d)

src/main/java/com/chrosciu/shop/products/ProductNotFoundExceptionMapper.java
@@ -1,0 +1,15 @@
+package com.chrosciu.shop.products;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ProductNotFoundExceptionMapper implements ExceptionMapper<ProductNotFoundException> {
+
+    @Override
+    public Response toResponse(ProductNotFoundException exception) {
+        return Response.status(Status.NO_CONTENT).build();
+    }
+}