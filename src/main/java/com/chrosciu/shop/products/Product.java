607b0e32fa6d328052558df102583198c4796256
Marcin Chrost <mchrost82@gmail.com>
2024-01-30 18:07:47 +0100

    Integration with database by JPA

--- src/main/java/com/chrosciu/shop/products/Product.java
+++ src/main/java/com/chrosciu/shop/products/Product.java
@@ -1,14 +1,28 @@
 package com.chrosciu.shop.products;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Builder
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
 public class Product implements Serializable {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String description;
